038acc0691b16099878737a1d2c6ddd5e55db7cc
Andreas Brekken <andreas@brekken.com>
2017-05-03 16:35:47 +0700

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+Automagically report errors and heartbeats
---
+++ index.js
@@ -0,0 +1,48 @@
+const {
+  SENTRY_DSN,
+  LYNX_HOST,
+  LYNX_PORT,
+} = process.env;
+
+const HEARTBEAT_INTERVAL = 10e3;
+
+const Raven = SENTRY_DSN && require('raven');
+const Lynx = LYNX_HOST && require('lynx');
+
+if (Raven) {
+  Raven.config(SENTRY_DSN, {
+    captureUnhandledRejections: true,
+  });
+
+  process.on('unhandledException', (error) => {
+    console.error('Unhandled exception:');
+    console.error(error.stack);
+    Raven.captureException(error, () => process.exit(1));
+  });
+}
+
+let lynx;
+let timer;
+
+if (Lynx) {
+  lynx = new Lynx(LYNX_HOST, +LYNX_PORT || 8125, {
+    on_error: error => {
+      console.error(error.stack);
+      if (!Raven) { return; }
+      Raven.captureException(e);
+    },
+  });
+
+  const packageName = require('./package.json').name;
+
+  timer = setInterval(() => {
+    lynx.increment(`service.heartbeat,service=${packageName}`);
+  }, HEARTBEAT_INTERVAL);
+}
+
+return {
+  Raven,
+  Lynx,
+  lynx,
+  timer,
+};
