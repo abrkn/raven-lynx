b8ca6831aef55355fef69accac16a78c8b387c04
Andreas Brekken <andreas@brekken.com>
2017-05-04 07:37:35 +0700

    Add debugging

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const debug = require('debug')('raven-lynx');
+
 const {
   SENTRY_DSN,
   LYNX_HOST,
@@ -11,6 +13,8 @@ const Lynx = LYNX_HOST && require('lynx');
 const parentPackageName = require('../../package.json').name;
 
 if (Raven) {
+  debug(`Configuring Raven with DSN ${SENTRY_DSN}`);
+
   Raven.config(SENTRY_DSN, {
     captureUnhandledRejections: true,
   });
@@ -26,6 +30,8 @@ let lynx;
 let timer;
 
 if (Lynx) {
+  debug(`Configuring Lynx with host ${LYNX_HOST}`);
+
   lynx = new Lynx(LYNX_HOST, +LYNX_PORT || 8125, {
     on_error: error => {
       console.error(error.stack);
@@ -34,7 +40,10 @@ if (Lynx) {
     },
   });
 
+  debug(`Will send heartbeat every ${HEARTBEAT_INTERVAL / 1e3} sec`);
+
   timer = setInterval(() => {
+    debug('Sending heartbeat...');
     lynx.increment(`service.heartbeat,service=${parentPackageName}`);
   }, HEARTBEAT_INTERVAL);
 }
